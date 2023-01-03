df2123539d45a0a57e0307d6312c62274f3e49bb
slzhang-git <92975752+slzhang-git@users.noreply.github.com>
2023-01-03 12:27:22 +0100

    Update NetFlowExt.py

--- NetFlowExt.py
+++ NetFlowExt.py
@@ -1,8 +1,8 @@
 import numpy as np
 import time
 from Logger import log
-from keras import backend as K
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+from tensorflow.compat.v1.keras import backend as K
 
 
 def dict_to_one(dp_dict={}):
